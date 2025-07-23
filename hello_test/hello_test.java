4c30367ead10e74a99bb3cfc5f599b9e64b25865
Mike P <25622379+mikeisahuman@users.noreply.github.com>
2025-07-23 13:12:21 -0600

    Add files via upload

---
+++ hello_test/hello_test.java
@@ -0,0 +1,11 @@
+// testing Java compilation, etc.
+
+public class hello_test {
+	public static void main(String[] args) {
+	// program begins
+	int x = 5;
+	String message = "\n\tHello from Java!\n";
+	String num = "\tThe special number is: " + x;
+	System.out.println(message + num + "\n");
+	}
+}
---
+++ hello_test/someclass.java
@@ -0,0 +1,12 @@
+// this is comment
+
+public class someclass {
+	public static void main (String[] args) {
+		System.out.println("\n\ttest from vim!\n");
+		if (1<0) {
+			int things = 0;
+		} else {
+			int things = 1;
+		}
+	}
+}
